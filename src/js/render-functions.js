56340d3f78b173cbc53003ca2d4ea347246e4b0f
masya <marinabondarska2006@gmail.com>
2026-03-26 20:19:33 +0100

    styles done

---
+++ src/js/render-functions.js
@@ -0,0 +1,41 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector('.gallery');
+const loader = document.querySelector('.loader');
+
+const lightbox = new SimpleLightbox('.gallery a', {});
+
+function itemTemplate(image) {
+  return `<li class="gallery-item">
+      <a href="${image.largeImageURL}">
+        <img src="${image.webformatURL}" alt="${image.tags}">
+      </a>
+      <ul class="stats">
+        <li class="stats-item"><p class="stat-title">likes</p><p class="stat-value">${image.likes}</p></li>
+        <li class="stats-item"><p class="stat-title">views</p><p class="stat-value">${image.views}</p></li>
+        <li class="stats-item"><p class="stat-title">comments</p><p class="stat-value">${image.comments}</p></li>
+        <li class="stats-item"><p class="stat-title">downloads</p><p class="stat-value">${image.downloads}</p></li>
+      </ul>
+    </li>`;
+}
+
+
+export function createGallery(images) {
+  console.log('createGallery started');
+  const result = images.map(itemTemplate).join('');
+  gallery.insertAdjacentHTML('beforeend', result);
+  lightbox.refresh();
+}
+
+export function clearGallery() {
+  gallery.innerHTML = '';
+}
+
+export function showLoader() {
+  loader.style.display = 'block';
+}
+
+export function hideLoader() {
+  loader.style.display = 'none';
+}
